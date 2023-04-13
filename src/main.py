dacb50f4babbdc0d453c525e77861a265cfad76d
jurra <jose.urra86@gmail.com>
2023-04-13 15:01:57 +0200

    cleaned main.cpp

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import pymurtree
+
+class PyMurtree():
+    def set_parameters():
+        pass
+
+    def fit():
+        pass
+
+    def test(self):
+        self.murtree = pymurtree.murtree("anneal.txt", 4, 15, 600)
+
+# pymurtree.murtree("anneal.txt", 4, 15, 600)
