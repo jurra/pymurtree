447c3c305eeb09c97cf6802a0a2c6e8a06324f2f
Yasel Quintero <y.i.quinterolares@tudelft.nl>
2023-06-02 20:11:54 +0200

    Load data from file with murtree format into numpy arrays

--- src/pymurtree/readdata.py
+++ src/pymurtree/readdata.py
@@ -1,4 +1,5 @@
 import pandas
+import numpy
 
 def read_from_file(path: str) -> tuple:
         """
@@ -16,4 +17,24 @@ def read_from_file(path: str) -> tuple:
         data = pandas.read_csv(path, sep=' ', header=None)
         y = data.iloc[:, 0]
         x = data.iloc[:, 1:]
+        return x, y
+
+
+def load_data(path: str) -> tuple:
+        """
+        Reads data from a space-separated text file with the following format:
+        The first column contains the target variable (y), and the remaining columns 
+        contain the features (x).
+
+        Args:
+            path (str): The path to the text file.
+
+        Returns:
+            tuple: A tuple containing:
+              x (numpy.ndarray): 2D array with all columns except the first 
+              y (numpy.ndarray): 1D array with the first column
+        """
+        data = numpy.loadtxt(path, dtype=int, delimiter=' ')
+        x = data[:, 1:]
+        y = data[:, 0]
         return x, y
